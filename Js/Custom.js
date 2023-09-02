3e11fa8a40d0ffd2ef0136c12225ecc1effac21d
Rana <76555387+HR-Rana@users.noreply.github.com>
2023-09-02 13:09:35 +0600

    Add files via upload

--- Js/Custom.js
+++ Js/Custom.js
@@ -2,8 +2,25 @@
 
 let Speech = new SpeechSynthesisUtterance();
 const Play = document.getElementById("Button");
+const Select = document.querySelector("select");
+
+let voices = [];
+
 
 Play.addEventListener('click', function(){
      Speech.text = document.getElementById("TextInput").value;
     window.speechSynthesis.speak(Speech);
 })
+
+window.speechSynthesis.onvoiceschanged = ()=>{
+    voices = window.speechSynthesis.getVoices();
+    Speech.voice = voices[0];
+
+    voices.forEach((voice, i)=>(Select.options[i] = new Option(voice.name, i)))
+}
+
+Select.addEventListener("change", ()=>{
+    Speech.voice = voices[Select.value];
+})
+
+
